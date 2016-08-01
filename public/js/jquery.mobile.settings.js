2fb871e3ba8b207c9e474bfae177b72dd27d8d12
Tim Visée <timvisee@gmail.com>
2016-08-01 17:39:01 +0200

    Clean jQuery mobile settings file

--- public/js/jquery.mobile.settings.js
+++ public/js/jquery.mobile.settings.js
@@ -1,15 +1,7 @@
-/**
- * Set some default jQuery Mobile settings for the current application.
- */
-
+// Set some default jQuery Mobile settings for the current application.
 $(document).bind("mobileinit", function(){
-    $.extend(  $.mobile , {
-        defaultPageTransition: "slide"
-        // , defaultTheme: "b"
+    // Set the default page transition
+    $.extend($.mobile, {
+        defaultPageTransition: 'slide'
     });
-
-    /*
-     $.mobile.page.prototype.options.contentTheme = "b";
-     $.mobile.page.prototype.options.theme = "b";
-     */
 });
