6b9d1b367da96e0e7d6d553418833459df3418d1
Tim Visée <timvisee@gmail.com>
2016-08-01 18:09:00 +0200

    Change front page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ var router = express.Router();
 var about = require('./about');
 var status = require('./status');
 
+var appInfo = require('../appInfo');
+
 // Index page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: appInfo.APP_NAME
+  });
 });
 
 // About page
