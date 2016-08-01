eee02fc07623f43e2fcc2436b49632bbeac5bb84
Tim Visée <timvisee@gmail.com>
2016-08-01 17:34:12 +0200

    Add about page content

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var appInfo = require('../appInfo');
+
 // About index
 router.get('/', function(req, res, next) {
     res.render('about', {
-        title: 'About'
+        title: 'About',
+
+        // TODO: Always include these parameters, in every page render
+        appName: appInfo.APP_NAME,
+        appVersionName: appInfo.VERSION_NAME,
+        appVersionCode: appInfo.VERSION_CODE
     });
 });
 
