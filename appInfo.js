eceb85bdf2a105ed72e9d9cb84753bf9d0508aef
Tim Visée <timvisee@gmail.com>
2016-08-01 14:58:05 +0200

    Create application info file

---
+++ appInfo.js
@@ -0,0 +1,56 @@
+/******************************************************************************
+ * Copyright (c) HHS OnTime 2016. All rights reserved.                        *
+ *                                                                            *
+ * @author Tim Visee                                                          *
+ * @website http://timvisee.com/                                              *
+ *                                                                            *
+ * Open Source != No Copyright                                                *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * You should have received a copy of The MIT License (MIT) along with this   *
+ * program. If not, see <http://opensource.org/licenses/MIT/>.                *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Application information base.                                              *
+ ******************************************************************************/
+var appInfo = {};
+
+
+/******************************************************************************
+ * Application name and version information.                                  *
+ ******************************************************************************/
+
+/**
+ * Application name.
+ *
+ * @constant {string}
+ */
+appInfo.APP_NAME = 'Dworek';
+
+/**
+ * Application version name.
+ *
+ * @constant {string}
+ */
+appInfo.VERSION_NAME = '0.0.0';
+
+/**
+ * Application version code.
+ *
+ * @constant {int}
+ */
+appInfo.VERSION_CODE = 1;
+
+
+// Export the configuration
+module.exports = appInfo;
