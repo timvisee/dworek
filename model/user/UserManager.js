2477cd2e1e299e38ee397b32440d298c28da477d
Tim Visée <timvisee@gmail.com>
2016-08-03 18:44:34 +0200

    Improve UserManager cryptography and documentation Improve JSDocs for UserManager class. Improve user credentials verification process. Use HashUtils class instead of hashing manually.

--- model/user/UserManager.js
+++ model/user/UserManager.js
@@ -20,29 +20,30 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.                *
  ******************************************************************************/
 
-var bcrypt = require('bcrypt');
-
-var config = require('../../config');
 var UserDatabase = require('./UserDatabase');
 var User = require('./User');
+var HashUtils = require('../hash/HashUtils');
 
 /**
- * Constructor.
+ * UserManager class.
  *
- * @returns {UserManager} UserManager instance.
+ * @class
+ * @constructor
  */
-var UserManager = function() { };
+var UserManager = function() {};
 
 /**
  * Get a user by it's username.
  *
  * @param username Username of the user.
- * @param {function} callback (err, {User|null} user) Callback with the user, or null.
+ * @param {UserManager~getUserByUsername} callback Callback with the user.
  */
 UserManager.getUserByUsername = function(username, callback) {
     // Return some user data
     UserDatabase.layerFetchFieldsFromDatabase({username: username}, {_id: true}, function(err, data) {
-        // TODO: Catch errors from err parameter!
+        // Pass along errors
+        if(err !== null)
+            callback(err, null);
 
         // Make sure any is returned, if not return false through the callback
         if(data.length == 0) {
@@ -50,16 +51,23 @@ UserManager.getUserByUsername = function(username, callback) {
             return;
         }
 
-        // Get the user data-*
-        var rawUserData = data[0];
+        // Get the user data
+        let rawUserData = data[0];
 
         // Get the user ID
-        var userId = rawUserData._id.toString();
+        // TODO: Should we convert the user ID to a (MongoDB) ObjectId instance
+        let userId = rawUserData._id.toString();
 
         // Get the user and call it back
         callback(null, new User(userId));
     });
 };
+
+/**
+ * @callback UserManager~getUserByUsername
+ * @param {Error|null} Error instance if an error occurred, null otherwise.
+ * @param {User|null} User instance, or null if no user was found.
+ */
 
 /**
  * Get the user by it's credentials.
@@ -68,7 +76,7 @@ UserManager.getUserByUsername = function(username, callback) {
  *
  * @param username Username of the user.
  * @param password Password of the user. (not hashed)
- * @param {function} callback (err, {User|null} user) Callback with the user, or null if the credentials were invalid.
+ * @param {UserManager~getUserByCredentialsCallback} callback Callback with the user, or null if the credentials were invalid.
  */
 UserManager.getUserByCredentials = function(username, password, callback) {
     // Make sure all fields are given
@@ -86,7 +94,7 @@ UserManager.getUserByCredentials = function(username, password, callback) {
         // Handle errors
         if(err != null) {
             if(callback != undefined)
-                callback(err);
+                callback(err, null);
             return;
         }
 
@@ -98,34 +106,22 @@ UserManager.getUserByCredentials = function(username, password, callback) {
         }
 
         // Get the user data
-        var rawUserData = data[0];
+        let rawUserData = data[0];
 
         // Gather the user ID, password hash and it's salt
-        var passwordHash = rawUserData.password_hash;
+        let passwordHash = rawUserData.password_hash;
 
-        // Get the global salt and hash round count
-        var globalSalt = config.user.salt;
-
-        // Salt the password with the global salt
-        var passwordSalted = password + globalSalt;
-
-        // TODO: Use this to hash passwords for new users
-        // var hashRounds = config.security.hashRounds;
-        // bcrypt.hash(passwordSalted, hashRounds, function(err, hash) {
-        //     //console.log('Hash: ' + hash);
-        // });
-
-        // Compare the password with the password hash
-        bcrypt.compare(passwordSalted, passwordHash, function(err, res) {
+        // Compare the password hash to the password
+        HashUtils.compare(password, passwordHash, function(err, matched) {
             // Handle errors
             if(err != null) {
                 if(callback != undefined)
-                    callback(err);
+                    callback(err, null);
                 return;
             }
 
             // Make sure the password is valid
-            if(!res) {
+            if(!matched) {
                 if(callback != undefined)
                     callback(null, null);
                 return;
@@ -137,6 +133,12 @@ UserManager.getUserByCredentials = function(username, password, callback) {
         });
     });
 };
+
+/**
+ * @callback UserManager~getUserByCredentialsCallback
+ * @param {Error|null} Error instance if an error occurred, null otherwise.
+ * @param {User|null} User instance, or null if no user was found.
+ */
 
 // Return the created class
 module.exports = UserManager;
