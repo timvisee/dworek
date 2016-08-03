09182818dc519868c956dda95dcb5cbcc38c333d
Tim Visée <timvisee@gmail.com>
2016-08-03 15:56:13 +0200

    Classified RedisUtils, improve JSDocs

--- model/redis/RedisUtils.js
+++ model/redis/RedisUtils.js
@@ -25,103 +25,125 @@
 var config = require('../../config');
 var redis = require('redis');
 
-// The Redis client instance
-var redisClient = null;
+/**
+ * @class
+ * @constructor
+ */
+var RedisUtils = function() {
+    /**
+     * Defines whether the Redis client is ready.
+     *
+     * @type {boolean} True if ready, false if not.
+     */
+    this.ready = false;
+
+    /**
+     * Redis client instance.
+     *
+     * @type {RedisClient} Redis client instance.
+     */
+    this.redisClient = null;
+};
 
 /**
- * Defines whether the Redis client is ready.
+ * Connect to Redis as configured in the configuration file.
  *
- * @type {boolean} True if ready, false if not.
+ * @param {RedisUtils~connectCallback} [callback] Called when a connection has been made, or when failed to connect.
  */
-var ready = false;
+RedisUtils.prototype.connect = function(callback) {
+    // Do not connect if Redis usage is disabled
+    if(!config.redis.enable) {
+        // Show a warning
+        console.log('Not connecting to Redis. Redis is disabled in the configuration.');
 
-// The actual module with it's methods
-module.exports = {
-    /**
-     * Connect to Redis as configured in the configuration file.
-     *
-     * @param [callback] (err, redisClient) Called when a connection has been made, or when failed to connect.
-     */
-    connect: function(callback) {
-        // Do not connect if Redis usage is disabled
-        if(!config.redis.enable) {
-            // Show a warning
-            console.log('Not connecting to Redis. Redis is disabled in the configuration.');
-
-            // Call the callback if available
-            if(callback != undefined)
-                callback(null, null);
-            return;
-        }
-
-        // Show a status message
-        console.log('Connecting to Redis...');
-
-        // Connect to the redis client
-        redisClient = redis.createClient(config.redis.url);
-
-        // Handle connect
-        redisClient.on('connect', function() {
-            // Show a status message
-            console.log('Successfully established a connection to Redis!');
-
-            // Reset the ready flag
-            ready = false;
-        });
-
-        // Handle ready
-        redisClient.on('ready', function(err) {
-            // Show a message
-            console.log('Redis is ready!');
-
-            // Set the ready flag
-            ready = true;
-
-            // Call the callback
-            if(callback != undefined)
-                callback(null, redisClient);
-        });
-
-        // Handle reconnecting
-        redisClient.on('reconnecting', function() {
-            // Reset the ready flag
-            ready = false;
-        });
-
-        // Handle errors
-        redisClient.on('error', function(err) {
-            // Show an error message
-            console.warn('Redis error: ' + err);
-
-            // Reset the ready flag
-            ready = false;
-
-            // Call the callback
-            if(callback != undefined)
-                callback(err);
-        });
-
-        // Handle end
-        redisClient.on('end', function() {
-            // Reset the ready flag
-            ready = false;
-        });
-    },
-
-    /**
-     * Get the Redis instance. A connection must have been established, or null will be returned.
-     * @returns {RedisClient}
-     */
-    getRedis: function() {
-        return redisClient;
-    },
-
-    /**
-     * Check whether the Redis client is ready.
-     *
-     * @returns {boolean} True if ready, false if not.
-     */
-    isReady: function() {
-        return ready;
+        // Call the callback if available
+        if(callback != undefined)
+            callback(null, null);
+        return;
     }
+
+    // Show a status message
+    console.log('Connecting to Redis...');
+
+    // Connect to the redis client
+    this.redisClient = redis.createClient(config.redis.url);
+
+    // Handle connect
+    this.redisClient.on('connect', function() {
+        // Show a status message
+        console.log('Successfully established a connection to Redis!');
+
+        // Reset the ready flag
+        this.ready = false;
+    });
+
+    // Store this instance
+    let instance = this;
+
+    // Handle ready
+    this.redisClient.on('ready', function(err) {
+        // TODO: Handle errors!
+
+        // Show a message
+        console.log('Redis is ready!');
+
+        // Set the ready flag
+        this.ready = true;
+
+        // Call the callback
+        if(callback != undefined)
+            callback(null, instance.redisClient);
+    });
+
+    // Handle reconnecting
+    this.redisClient.on('reconnecting', function() {
+        // Reset the ready flag
+        this.ready = false;
+    });
+
+    // Handle errors
+    this.redisClient.on('error', function(err) {
+        // Show an error message
+        console.warn('Redis error: ' + err);
+
+        // Reset the ready flag
+        this.ready = false;
+
+        // Call the callback
+        if(callback != undefined)
+            callback(err, undefined);
+    });
+
+    // Handle end
+    this.redisClient.on('end', function() {
+        // Reset the ready flag
+        this.ready = false;
+    });
 };
+
+/**
+ * @callback RedisUtils~connectCallback
+ * @param {Error|null} Error instance if an error occurred, null otherwise.
+ * @param {RedisClient} Redis client instance with the connection if the connection was successful.
+ */
+
+/**
+ * Get the Redis instance. A connection must have been established, or null will be returned.
+ *
+ * @returns {RedisClient} RedisClient instance.
+ */
+RedisUtils.prototype.getRedis = function() {
+    return this.redisClient;
+};
+
+/**
+ * Check whether the Redis client is ready.
+ *
+ * @returns {boolean} True if ready, false if not.
+ */
+RedisUtils.prototype.isReady = function() {
+    return this.ready;
+};
+
+// Export the class
+module.exports = RedisUtils;
