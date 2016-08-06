75325984cde198ee0e40061ca71a47a48bc0b1bd
Tim Visée <timvisee@gmail.com>
2016-08-06 13:21:06 +0200

    Move setups to Core class

--- server/App.js
+++ server/App.js
@@ -20,18 +20,7 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.                *
  ******************************************************************************/
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./app/route/index');
-
-var appInfo = require('./appInfo');
-var MongoUtils = require('./app/mongo/MongoUtils');
-var RedisUtils = require('./app/redis/RedisUtils');
+var Core = require('./Core');
 
 /**
  * Constructor.
@@ -42,11 +31,6 @@ var RedisUtils = require('./app/redis/RedisUtils');
  * @param {boolean} [init] True to immediately initialize.
  */
 var App = function(init) {
-    /**
-     * Express application instance.
-     */
-    this.expressApp = null;
-
     // Initialize
     if(init != undefined && init)
         this.init();
@@ -54,145 +38,15 @@ var App = function(init) {
 
 /**
  * Initialize the application.
- * This will start the application instance, database connection, router and so on.
+ * This will start the application and it's core, initiating things like the database, Redis and the router.
  */
 App.prototype.init = function() {
-    // Create an Express application instance
-    this.expressApp = express();
+    // Make sure the core hasn't been initialized yet
+    if(Core.isInit())
+        return;
 
-    // Disable express branding in HTTP responses
-    this.expressApp.disable('x-powered-by');
-
-    // Create an app instance to make it accessible from the callback
-    var instance = this;
-
-    // Connect to the database
-    instance.connectDatabase(function() {
-        // Connect to Redis
-        instance.connectRedis();
-
-        // Start the router
-        instance.startRouter();
-    });
-};
-
-/**
- * Connect to the database.
- *
- * @param {App~connectDatabaseCallback} callback Callback.
- */
-App.prototype.connectDatabase = function(callback) {
-    // Connect to the database
-    MongoUtils.connect(function(err, db) {
-        // Handle errors
-        if(err != null)
-            throw new Error('Failed to connect to MongoDB. Quitting server.');
-
-        // TODO: Retry on connection failure
-
-        // Callback
-        callback(null, db);
-    });
-};
-
-/**
- * Callback, called when a connection to the database has been made.
- *
- * @callback App~connectDatabaseCallback
- * @param {Error|null} Error instance if an error occurred, null otherwise.
- * @param {MongoClient|null} MongoDB client instance if a connection has been made, null otherwise.
- */
-
-/**
- * Connect to Redis.
- *
- * @param {App~connectRedisCallback} [callback] Callback.
- */
-App.prototype.connectRedis = function(callback) {
-    // Connect to Redis
-    RedisUtils.connect(callback);
-};
-
-/**
- * Callback, called when Redis connected or failed to connect.
- *
- * @callback App~connectRedisCallback
- * @param {Error|null} Error instance if an error occurred, null otherwise.
- * @param {RedisClient|null} Redis client instance if a connection has been made, null otherwise.
- */
-
-/**
- * Start the router.
- */
-App.prototype.startRouter = function() {
-    // Show a status message
-    console.log("Starting router...");
-
-    // Configure the view engine
-    this.expressApp.set('views', path.join(__dirname, './views'));
-    this.expressApp.set('view engine', 'jade');
-
-    // Configure the favicon
-    // TODO: Configure static all favicons here, instead of the default one
-    this.expressApp.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
-    this.expressApp.use(logger('dev'));
-    this.expressApp.use(bodyParser.json());
-    this.expressApp.use(bodyParser.urlencoded({extended: false}));
-    this.expressApp.use(cookieParser());
-    this.expressApp.use(express.static(path.join(__dirname, '../public')));
-
-    // Configuring route
-    console.log("Configuring router...");
-
-    // Add application branding in HTTP responses
-    this.expressApp.use(function(req, res, next) {
-        // Set the HTTP X-Powered-By header
-        res.header('X-Powered-By', appInfo.APP_NAME + ' Server/' + appInfo.VERSION_NAME);
-
-        // Route to the next handler
-        next();
-    });
-
-    // Configure the router
-    this.expressApp.use('/', routes);
-
-    // Catch 404 errors, and forward them to the error handler
-    this.expressApp.use(function(req, res, next) {
-        // Create an error, and set the status code
-        var error = new Error('Not Found');
-        error.status = 404;
-
-        // Forward the error
-        next(error);
-    });
-
-    // Store this instance
-    var instance = this;
-
-    // Error handler
-    this.expressApp.use(function(err, req, res, next) {
-        // Determine whether we're in development mode
-        var dev = instance.expressApp.get('env') === 'development';
-
-        // Show an error page, render the stack trace if we're in development mode
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: dev ? err : {}
-        });
-    });
-
-    // Show a status message
-    console.log("Router started");
-};
-
-/**
- * Get the Express application instance.
- *
- * @returns {*}
- */
-App.prototype.getExpressApp = function() {
-    return this.expressApp;
+    // Initialize the core
+    Core.init();
 };
 
 // Export the class
--- server/Core.js
+++ server/Core.js
@@ -20,7 +20,18 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.                *
  ******************************************************************************/
 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var appInfo = require('./appInfo');
+var routes = require('./app/route/index');
 var GameController = require('./app/game/GameController');
+var MongoUtils = require('./app/mongo/MongoUtils');
+var RedisUtils = require('./app/redis/RedisUtils');
 
 /**
  * Core class.
@@ -39,14 +50,34 @@ var Core = function() {};
 Core._init = false;
 
 /**
+ * Game controller instance.
+ *
+ * @type {GameController|null} Game controller instance, or null if the core hasn't been initialized.
+ */
+Core.gameController = null;
+
+/**
+ * Express app instance.
+ *
+ * @type {*} Express app.
+ */
+Core.expressApp = null;
+
+/**
  * Initialize the Core.
  */
 Core.init = function() {
     // Initialize the game controller
-    Core.gameController = Object.create(GameController);
+    this._initGameController();
 
-    // Load all active games
-    Core.gameController.loadActiveGames();
+    // Initialize the express application
+    this._initExpressApp();
+
+    // Initialize the database
+    this._initDatabase();
+
+    // Initialize Redis
+    this._initRedis();
 
     // Set the initialization status
     Core._init = true;
@@ -62,11 +93,126 @@ Core.isInit = function() {
 };
 
 /**
- * Game controller instance.
+ * Initialize the game controller.
  *
- * @type {GameController|null} Game controller instance, or null if the core hasn't been initialized.
+ * @private
  */
-Core.gameController = null;
+Core._initGameController = function() {
+    // Initialize the game controller
+    Core.gameController = Object.create(GameController);
+
+    // Load all active games
+    Core.gameController.loadActiveGames();
+};
+
+/**
+ * Initialize the express app.
+ *
+ * @private
+ */
+Core._initExpressApp = function() {
+    // Create an Express application instance
+    Core.expressApp = express();
+
+    // Disable express branding in HTTP responses
+    Core.expressApp.disable('x-powered-by');
+
+    // TODO: Move the following code to a router class
+
+    // Show a status message
+    console.log('Starting router...');
+
+    // Configure the view engine
+    this.expressApp.set('views', path.join(__dirname, './views'));
+    this.expressApp.set('view engine', 'jade');
+
+    // Configure the favicon
+    // TODO: Configure static all favicons here, instead of the default one
+    this.expressApp.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+    this.expressApp.use(logger('dev'));
+    this.expressApp.use(bodyParser.json());
+    this.expressApp.use(bodyParser.urlencoded({extended: false}));
+    this.expressApp.use(cookieParser());
+    this.expressApp.use(express.static(path.join(__dirname, '../public')));
+
+    // Configuring route
+    console.log("Configuring router...");
+
+    // Add application branding in HTTP responses
+    this.expressApp.use(function(req, res, next) {
+        // Set the HTTP X-Powered-By header
+        res.header('X-Powered-By', appInfo.APP_NAME + ' Server/' + appInfo.VERSION_NAME);
+
+        // Route to the next handler
+        next();
+    });
+
+    // Configure the router
+    this.expressApp.use('/', routes);
+
+    // Catch 404 errors, and forward them to the error handler
+    this.expressApp.use(function(req, res, next) {
+        // Create an error, and set the status code
+        var error = new Error('Not Found');
+        error.status = 404;
+
+        // Forward the error
+        next(error);
+    });
+
+    // Store this instance
+    var instance = this;
+
+    // Error handler
+    this.expressApp.use(function(err, req, res, next) {
+        // Determine whether we're in development mode
+        var dev = instance.expressApp.get('env') === 'development';
+
+        // Show an error page, render the stack trace if we're in development mode
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: dev ? err : {}
+        });
+    });
+
+    // Show a status message
+    console.log('Router started');
+};
+
+/**
+ * Initialize the database and connect.
+ *
+ * @param {function} [callback] Called when a connection has been made, or if an error occurred.
+ * @private
+ */
+Core._initDatabase = function(callback) {
+    // Connect to the database
+    MongoUtils.connect(function(err, db) {
+        // Handle errors
+        if(err != null)
+            throw new Error('Failed to connect to MongoDB. Quitting server.');
+
+        // TODO: Retry on connection failure
+
+        // Callback
+        if(callback !== undefined)
+            callback(null, db);
+    });
+};
+
+/**
+ * Initialize Redis and connect.
+ *
+ * @param {function} [callback] Called when a connection has been made, or if an error occurred.
+ * @private
+ */
+Core._initRedis = function(callback) {
+    // Connect to Redis
+    RedisUtils.connect(function(err) {
+        callback(err);
+    });
+};
 
 // Export the class
 module.exports = Core;
