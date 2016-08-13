1b187fbc0d4d6a103c1789183a14056a9c8ee8e0
Tim Visée <timvisee@gmail.com>
2016-08-13 17:10:08 +0200

    Move Core logic to App class (breaking commit)

--- server/App.js
+++ server/App.js
@@ -20,7 +20,15 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.                *
  ******************************************************************************/
 
+var async = require('async');
+var express = require('express');
+
 var Core = require('./Core');
+var GameController = require('./app/game/GameController');
+var MongoUtils = require('./app/mongo/MongoUtils');
+var RedisUtils = require('./app/redis/RedisUtils');
+var Router = require('./app/router/Router');
+var PathLibrary = require('./PathLibrary');
 
 /**
  * Constructor.
@@ -32,11 +40,33 @@ var Core = require('./Core');
  */
 var App = function(init) {
     /**
-     * Core instance.
+     * Define whether the App is initialized.
      *
-     * @type {Core}
+     * @type {boolean} True if initialized, false if not.
+     * @private
      */
-    this.core = null;
+    this._init = false;
+
+    /**
+     * Router instance.
+     *
+     * @type {Router}
+     */
+    this.router = null;
+
+    /**
+     * Game controller instance.
+     *
+     * @type {GameController|null} Game controller instance, or null if the core hasn't been initialized.
+     */
+    this.gameController = null;
+
+    /**
+     * Express app instance.
+     *
+     * @type {*} Express app.
+     */
+    this.expressApp = null;
 
     // Initialize
     if(init != undefined && init)
@@ -47,21 +77,153 @@ var App = function(init) {
  * Initialize the application.
  * This will start the application and it's core, initiating things like the database, Redis and the router.
  *
- * @param {function} [callback] Called when finished initializing, or when an error occurred.
+ * @param {function} [initCallback] Called when finished initializing, or when an error occurred.
  */
-App.prototype.init = function(callback) {
+App.prototype.init = function(initCallback) {
     // Create a core instance if it hasn't been instantiated yet
     if(this.core === null)
         this.core = new Core();
 
-    // Call back if the core is already initialized
-    if(this.core.isInit()) {
-        callback(null);
-        return;
-    }
+    // Store the instance
+    const instance = this;
 
-    // Initialize the core
-    this.core.init(callback);
+    // Initialize various components in parallel
+    async.parallel([
+        (callback) => {
+            // Print the library paths, and call back
+            PathLibrary.printPaths();
+            callback(null);
+        },
+
+        // TODO: Can we rename this parameter, to make the init() parameter consistent?
+        (callback) =>
+            // Initialize the game controller
+            instance._initGameController(callback),
+
+        // TODO: Can we rename this parameter, to make the init() parameter consistent?
+        (callback) =>
+            // Initialize the express application
+            instance._initExpressApp(callback),
+
+        // TODO: Can we rename this parameter, to make the init() parameter consistent?
+        (callback) =>
+            // Initialize the database
+            instance._initDatabase(callback),
+
+        // TODO: Can we rename this parameter, to make the init() parameter consistent?
+        (callback) =>
+            // Initialize Redis
+            instance._initRedis(callback)
+
+    ], function(err) {
+        // Make sure everything went right, callback or throw an error instead
+        if(err !== null) {
+            if(initCallback !== undefined)
+                initCallback(err);
+            else
+                throw err;
+            return;
+        }
+
+        // Initialize the router
+        //noinspection JSAccessibilityCheck
+        instance._initRouter(function(err) {
+            // Call back any errors, or throw it if no callback was defined
+            if(err !== null) {
+                if(initCallback !== undefined)
+                    initCallback(err);
+                else
+                    throw err;
+            }
+
+            // Set the initialization status
+            instance._init = true;
+
+            // Call back
+            if(initCallback !== undefined)
+                initCallback(null);
+        });
+    });
+};
+
+/**
+ * Check whether the app is initialized.
+ *
+ * @return {boolean} True if initialized, false if not.
+ */
+App.prototype.isInit = function() {
+    return this._init;
+};
+
+/**
+ * Initialize the game controller.
+ */
+App.prototype._initGameController = function(callback) {
+    // Initialize the game controller
+    this.gameController = new GameController();
+
+    // Load all active games
+    this.gameController.loadActiveGames(callback);
+};
+
+/**
+ * Initialize the express app.
+ *
+ * @param {function} [callback] Called when finished initializing, or when an error occurred.
+ */
+App.prototype._initExpressApp = function(callback) {
+    // Create an Express application instance
+    this.expressApp = express();
+
+    // Disable express branding in HTTP responses
+    this.expressApp.disable('x-powered-by');
+
+    // We're done, call back if a callback is defined
+    if(callback !== undefined)
+        callback(null);
+};
+
+/**
+ * Initialize the router.
+ *
+ * @param {function} [callback] Called when the router has been initialized.
+ */
+App.prototype._initRouter = function(callback) {
+    // Set the router instance, and initialize
+    this.router = new Router(this, false);
+
+    // Initialize
+    this.router.init(callback);
+};
+
+/**
+ * Initialize the database and connect.
+ *
+ * @param {function} [callback] Called when a connection has been made, or if an error occurred.
+ */
+App.prototype._initDatabase = function(callback) {
+    // Connect to the database
+    MongoUtils.connect(function(err) {
+        // Handle errors
+        if(err !== null)
+            throw new Error('Failed to connect to MongoDB. Quitting server.');
+
+        // TODO: Retry on connection failure
+
+        // We're done, call back if a callback is defined
+        if(callback !== undefined)
+            callback(null);
+    });
+};
+
+/**
+ * Initialize Redis and connect.
+ *
+ * @param {function} [callback] Called when a connection has been made, or if an error occurred.
+ */
+App.prototype._initRedis = function(callback) {
+    // Connect to Redis
+    RedisUtils.connect(callback);
 };
 
 // Export the class
