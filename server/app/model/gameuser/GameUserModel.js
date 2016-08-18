56ecb3e562a7395dac8709dd2e204d4a00477aad
Tim Visée <timvisee@gmail.com>
2016-08-19 01:51:39 +0200

    Create GameUserModel class basis (WIP)

---
+++ server/app/model/gameuser/GameUserModel.js
@@ -0,0 +1,284 @@
+/******************************************************************************
+ * Copyright (c) Dworek 2016. All rights reserved.                            *
+ *                                                                            *
+ * @author Tim Visee                                                          *
+ * @website http://timvisee.com/                                              *
+ *                                                                            *
+ * Open Source != No Copyright                                                *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * You should have received a copy of The MIT License (MIT) along with this   *
+ * program. If not, see <http://opensource.org/licenses/MIT/>.                *
+ ******************************************************************************/
+
+var util = require('util');
+
+var Core = require('../../../Core');
+var GameUserDatabase = require('./GameUserDatabase');
+var BaseModel = require('../../db/BaseModel');
+var DatabaseObjectLayer = require('../../database/DatabaseObjectLayer');
+var UserModel = require('../user/UserModel');
+var ConversionFunctions = require('../../db/ConversionFunctions');
+
+/**
+ * GameUserModel class.
+ *
+ * @class
+ * @constructor
+ *
+ * @param {ObjectId} id Game user ID object.
+ *
+ * @returns {GameUserModel} Game user instance.
+ */
+var GameUserModel = function(id) {
+    /**
+     * Set the API application ID.
+     *
+     * @private
+     */
+    this._id = id;
+
+    // Create and configure the base model instance for this model
+    this._baseModel = new BaseModel(this, {
+        mongo: {
+            collection: GameUserDatabase.DB_COLLECTION_NAME
+        },
+        fields: {
+            game: {
+                mongo: {
+                    field: 'game_id',
+
+                    /**
+                     * Convert an ID to an Game model.
+                     *
+                     * @param {ObjectId} id
+                     * @return {GameModel} Game.
+                     */
+                    from: (id) => Core.model.gameModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an Game model to an ID.
+                     *
+                     * @param {GameModel} game Game.
+                     * @return {ObjectId} ID.
+                     */
+                    to: (game) => game.getId()
+                },
+                cache: {
+                    /**
+                     * Convert a hexadecimal ID to a Game model.
+                     *
+                     * @param {String} id
+                     * @return {GameModel} Game.
+                     */
+                    from: (id) => Core.model.gameModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an Game model to a hexadecimal ID.
+                     *
+                     * @param {GameModel} game Game.
+                     * @return {String} Hexadecimal ID.
+                     */
+                    to: (game) => game.getIdHex()
+                },
+                redis: {
+                    /**
+                     * Convert a hexadecimal ID to a Game model.
+                     *
+                     * @param {String} id
+                     * @return {GameModel} Game.
+                     */
+                    from: (id) => Core.model.gameModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an Game model to a hexadecimal ID.
+                     *
+                     * @param {GameModel} game Game.
+                     * @return {String} Hexadecimal ID.
+                     */
+                    to: (game) => game.getIdHex()
+                }
+            },
+            user: {
+                mongo: {
+                    field: 'user_id',
+
+                    /**
+                     * Convert an ID to an User model.
+                     *
+                     * @param {ObjectId} id
+                     * @return {UserModel} User.
+                     */
+                    from: (id) => Core.model.userModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an User model to an ID.
+                     *
+                     * @param {UserModel} user User.
+                     * @return {ObjectId} ID.
+                     */
+                    to: (user) => user.getId()
+                },
+                redis: {
+                    /**
+                     * Convert a hexadecimal ID to a User model.
+                     *
+                     * @param {String} id
+                     * @return {UserModel} User.
+                     */
+                    from: (id) => Core.model.userModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an User model to a hexadecimal ID.
+                     *
+                     * @param {UserModel} user User.
+                     * @return {String} Hexadecimal ID.
+                     */
+                    to: (user) => user.getIdHex()
+                }
+            },
+            team: {
+                mongo: {
+                    field: 'team_id',
+
+                    /**
+                     * Convert an ID to an Game Team model.
+                     *
+                     * @param {ObjectId} id
+                     * @return {GameTeamModel} Game Team.
+                     */
+                    from: (id) => Core.model.teamModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an Game Team model to an ID.
+                     *
+                     * @param {GameTeamModel} team Game Team.
+                     * @return {ObjectId} ID.
+                     */
+                    to: (team) => team.getId()
+                },
+                redis: {
+                    /**
+                     * Convert a hexadecimal ID to a Game Team model.
+                     *
+                     * @param {String} id
+                     * @return {GameTeamModel} Team.
+                     */
+                    from: (id) => Core.model.gameTeamModelManager._instanceManager.create(id),
+
+                    /**
+                     * Convert an Game Team model to a hexadecimal ID.
+                     *
+                     * @param {GameTeamModel} team Game Team.
+                     * @return {String} Hexadecimal ID.
+                     */
+                    to: (team) => team.getIdHex()
+                }
+            },
+            is_special: {
+                redis: {
+                    /**
+                     * Convert the string value to a boolean.
+                     *
+                     * @param {string} bool Boolean as a string.
+                     * @return {boolean} Boolean value.
+                     */
+                    from: (bool) => bool != '0',
+
+                    /**
+                     * Convert the boolean value to a string.
+                     *
+                     * @param {boolean} bool Boolean value.
+                     * @return {string} Boolean as a string.
+                     */
+                    to: (bool) => bool ? 1 : 0
+                }
+            },
+            is_spectator: {
+                redis: {
+                    /**
+                     * Convert the string value to a boolean.
+                     *
+                     * @param {string} bool Boolean as a string.
+                     * @return {boolean} Boolean value.
+                     */
+                    from: (bool) => bool != '0',
+
+                    /**
+                     * Convert the boolean value to a string.
+                     *
+                     * @param {boolean} bool Boolean value.
+                     * @return {string} Boolean as a string.
+                     */
+                    to: (bool) => bool ? 1 : 0
+                }
+            }
+        }
+    });
+};
+
+/**
+ * Get the ID object of the game.
+ *
+ * @returns {ObjectId} Game ID object.
+ */
+GameUserModel.prototype.getId = function() {
+    return this._id;
+};
+
+/**
+ * Get the hexadecimal ID representation of the game.
+ *
+ * @returns {*} Game ID as hexadecimal string.
+ */
+GameUserModel.prototype.getIdHex = function() {
+    return this.getId().toString();
+};
+
+/**
+ * Get the given field from the model.
+ *
+ * @param {String} field Field names.
+ * @param {GameUserModel~getFieldCallback} callback Called with the result of a model field, or when an error occurred.
+ */
+GameUserModel.prototype.getField = function(field, callback) {
+    this._baseModel.getField(field, callback);
+};
+
+/**
+ * Called with the result of a model field, or when an error occurred.
+ *
+ * @callback GameModel~getFieldCallback
+ * @param {Error|null} Error instance if an error occurred, null otherwise.
+ * @param {*=} Field value.
+ */
+
+/**
+ * Set the given field to the given value for this model.
+ *
+ * @param {String} field Field name.
+ * @param {*} value Field value.
+ * @param {GameUserModel~setFieldCallback} callback Called on success, or when an error occurred.
+ */
+GameUserModel.prototype.setField = function(field, value, callback) {
+    this._baseModel.setField(field, value, callback);
+};
+
+/**
+ * Called on success, or when an error occurred.
+ *
+ * @callback GameModel~setFieldCallback
+ * @param {Error|null} Error instance if an error occurred, null on success.
+ */
+
+// Export the user class
+module.exports = GameUserModel;
