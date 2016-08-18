579bd46a87c67ec9e29e4bd33cfc2ac1ffe4aa1b
Tim Visée <timvisee@gmail.com>
2016-08-18 22:18:28 +0200

    Show list of games on dashboard's games tab

--- server/app/route/index.js
+++ server/app/route/index.js
@@ -32,16 +32,59 @@ var about = require('./about');
 var status = require('./status');
 
 var appInfo = require('../../appInfo');
+var Core = require('../../Core');
+var CallbackLatch = require('../util/CallbackLatch');
 var LayoutRenderer = require('../layout/LayoutRenderer');
 
 // Index page
 router.get('/', function(req, res, next) {
     // Show the index page if the user isn't logged in, show the dashboard if logged in
-    if(!req.session.valid)
+    if(!req.session.valid) {
         LayoutRenderer.render(req, res, next, 'index', appInfo.APP_NAME);
+        return;
+    }
 
-    else
-        LayoutRenderer.render(req, res, next, 'dashboard', appInfo.APP_NAME);
+    // Create a callback latch
+    var latch = new CallbackLatch();
+
+    // Create an object with layout options
+    var options = {
+        games: {
+            games: {
+                open: [],
+                active: []
+            }
+        }
+    };
+
+    // Count the games
+    latch.add();
+    latch.add();
+    getGameList(0, 3, function(err, games) {
+        // Call back errors
+        if(err !== null)
+            next(err);
+        else
+            options.games.games.open = games;
+
+        // Resolve the latch
+        latch.resolve();
+    });
+    getGameList(1, 3, function(err, games) {
+        // Call back errors
+        if(err !== null)
+            next(err);
+        else
+            options.games.games.active = games;
+
+        // Resolve the latch
+        latch.resolve();
+    });
+
+    // Render the games page
+    latch.then(function() {
+        LayoutRenderer.render(req, res, next, 'dashboard', appInfo.APP_NAME, options);
+    });
 });
 
 // Login page
@@ -64,5 +107,70 @@ router.use('/about', about);
 
 // Status page
 router.use('/status', status);
+
+/**
+ * Get the game list.
+ *
+ * @param {Number} stage Game stage.
+ * @param {Number|undefined} limit Limit of games to fetch, undefined to fetch all.
+ * @param {function} callback Callback(err, games)
+ */
+function getGameList(stage, limit, callback) {
+    // Create a list of usable game objects to use for rendering
+    var gameObjects = [];
+
+    // Create a callback latch to determine whether to start rendering the layout
+    var latch = new CallbackLatch();
+
+    // Get the list of active games
+    latch.add();
+    Core.model.gameModelManager.getGamesWithStage(stage, {
+        limit
+    }, function(err, games) {
+        // Call back errors
+        if(err !== null) {
+            callback(err);
+            return;
+        }
+
+        // Fill the list of game objects
+        games.forEach(function(game) {
+            // Add a callback latch for this entry
+            latch.add();
+
+            // Create a dummy game object
+            var gameObject = {
+                id: game.getIdHex(),
+                name: null
+            };
+
+            // Get the game name and put it in the object
+            game.getName(function(err, name) {
+                // Call back errors
+                if(err !== null) {
+                    callback(err);
+                    return;
+                }
+
+                // Set the name
+                gameObject.name = name;
+
+                // Add the game object to the list
+                gameObjects.push(gameObject);
+
+                // Resolve the latch
+                latch.resolve();
+            });
+        });
+
+        // Resolve the latch
+        latch.resolve();
+    });
+
+    // Call back the list
+    latch.then(function() {
+        callback(null, gameObjects);
+    });
+}
 
 module.exports = router;
