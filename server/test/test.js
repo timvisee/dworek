11c0e2f604f675bf4653739ea80035ab6ad1aab1
Tim Visée <timvisee@gmail.com>
2016-08-22 18:00:00 +0200

    Remove unused dummy test code, along with it's dependencies

--- server/test/test.js
+++ server/test/test.js
@@ -23,58 +23,10 @@
 // Inspired by:
 // http://developmentnow.com/2015/02/05/make-your-node-js-api-bulletproof-how-to-test-with-mocha-chai-and-supertest/
 
-/**
- * REST API host.
- *
- * @type {string} Host.
- */
-const API_HOST = 'http://localhost:3000';
-
-/**
- * REST API path.
- *
- * @type {string} Path.
- */
-const API_URL = '/api/v1';
-
-
-
 var mocha = require('mocha');
 var it = mocha.it;
-var chai = require('chai');
-var supertest = require('supertest');
-var api = supertest(API_HOST);
-
-
-
-/**
- * Test the given endpoint.
- *
- * @param endpoint Endpoint path.
- *
- * @returns {Object} SuperTest endpoint request.
- */
-function testEndpoint(endpoint) {
-    // Make sure the endpoint starts with a slash, prefix it if that's not the case
-    if(endpoint.charAt(0) !== '/')
-        endpoint = '/' + endpoint;
-
-    // Create the endpoint request
-    var request = api.get(API_URL + endpoint);
-
-    // Accept JSON
-    request.set('Accept', 'application/json');
-
-    // Return the request
-    return request;
-}
 
 // Dummy test
 it('Dummy test.', function(done) {
     done();
 });
-
-// // Make sure the API returns with a response code of 200
-// it('Should give a 200 response.', function(done) {
-//     testEndpoint('/test').expect(200, done);
-// });
