5460c4b1bb5d28a8913f150bd4ad85b956e0ea3f
Tim Visée <timvisee@gmail.com>
2016-08-23 21:23:54 +0200

    Fix main login page being rendered twice

--- server/app/route/login.js
+++ server/app/route/login.js
@@ -40,7 +40,7 @@ router.get('/', function(req, res, next) {
     }
 
     // Show the login page
-    res.render('login', LayoutRenderer.render(req, res, next, 'login'));
+    LayoutRenderer.render(req, res, next, 'login');
 });
 
 // Login index
