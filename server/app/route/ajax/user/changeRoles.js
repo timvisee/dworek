599b3b601c2933c272ce03e7ff83d7bb40da51c8
Tim Visée <timvisee@gmail.com>
2016-08-30 14:56:46 +0200

    Validate user permissions before chaning user roles via AJAX endpoint

--- server/app/route/ajax/user/changeRoles.js
+++ server/app/route/ajax/user/changeRoles.js
@@ -34,7 +34,8 @@ router.post('/', function(req, res, next) {
         return;
     }
 
-    // TODO: Make sure the user has rights to change this. (Check whether the user is the game host, or is administrator)
+    // Get the current user
+    const user = req.session.user;
 
     // Make sure a data object is given
     if(!req.body.hasOwnProperty('data')) {
@@ -52,11 +53,11 @@ router.post('/', function(req, res, next) {
     const isSpecial = data.role.special;
     const isSpectator = data.role.spectator;
 
-    // Create a callback latch for the role changing process
-    var latch = new CallbackLatch();
-
     // Create a variable for the game
     var game = null;
+
+    // Create a callback latch
+    var latch = new CallbackLatch();
 
     // Get the
     latch.add();
@@ -90,69 +91,74 @@ router.post('/', function(req, res, next) {
         // Reset the latch back to it's identity to recycle it
         latch.identity();
 
-        // Determine whether this request is cancelled, due to an error of some sort
-        var cancelled = false;
+        // Create flags to define whether the user is admin or host of the game
+        var admin = false;
+        var host = false;
 
-        // Create an array of players that were successfully updated
-        var updatedUsers = [];
-
-        // Loop through the user ids
-        users.forEach(function(userId) {
-            // Format the user ID
-            userId = userId.trim();
-
-            // Cancel
-            if(cancelled)
+        // Check whether the user is administrator
+        latch.add();
+        user.isAdmin(function(err, isAdmin) {
+            // Call back errors
+            if(err !== null) {
+                next(err);
                 return;
+            }
 
-            // Get the game user for this game and user
-            latch.add();
-            Core.model.gameUserModelManager.getGameUser(game, userId, function(err, gameUser) {
-                // Call back errors
-                if(err !== null) {
-                    if(!cancelled)
-                        next(err);
-                    cancelled = true;
+            // Set the admin flag
+            admin = isAdmin;
+
+            // Resolve the latch
+            latch.resolve();
+        });
+
+        // Check whether the user is host of the game
+        latch.add();
+        game.getUser(function(err, gameHost) {
+            // Call back errors
+            if(err != null) {
+                next(err);
+                return;
+            }
+
+            // Make sure the host user isn't null
+            if(gameHost === null) {
+                host = false;
+                return;
+            }
+
+            // Determine whether the user is host, and set the flag
+            host = gameHost.getId().equals(user.getId());
+        });
+
+        // Continue when we're done
+        latch.then(function() {
+            // Reset the latch back to it's identity
+            latch.identity();
+
+            // Make sure the user is host or administrator
+            if(!host && !admin) {
+                next(new Error('You don\'t have permission to change user roles'));
+                return;
+            }
+
+            // Determine whether this request is cancelled, due to an error of some sort
+            var cancelled = false;
+
+            // Create an array of players that were successfully updated
+            var updatedUsers = [];
+
+            // Loop through the user ids
+            users.forEach(function(userId) {
+                // Format the user ID
+                userId = userId.trim();
+
+                // Cancel
+                if(cancelled)
                     return;
-                }
 
-                // Make sure the game user is valid
-                if(gameUser === null || gameUser === undefined) {
-                    // Respond with an error
-                    if(!cancelled)
-                        res.json({
-                            status: 'error',
-                            error: {
-                                message: 'Invalid game and user ID combination for user \'' + userId + '\''
-                            }
-                        });
-
-                    // Set the cancelled flag and return
-                    cancelled = true;
-                    return;
-                }
-
-                // Determine the new team value
-                var newTeam;
-
-                // Set the new team value to none
-                if(teamValue === 'none' || teamValue === '')
-                    newTeam = null;
-
-                else
-                    // TODO: Determine the new team value (random, and team IDs)
-                    newTeam = null;
-
-                // Create a fields object with the new field values
-                const fields = {
-                    team: newTeam,
-                    is_special: isSpecial,
-                    is_spectator: isSpectator
-                };
-
-                // Set the users special state
+                // Get the game user for this game and user
                 latch.add();
-                gameUser.setFields(fields, function(err) {
+                Core.model.gameUserModelManager.getGameUser(game, userId, function(err, gameUser) {
                     // Call back errors
                     if(err !== null) {
                         if(!cancelled)
@@ -161,26 +167,72 @@ router.post('/', function(req, res, next) {
                         return;
                     }
 
-                    // Add the user to the updated users list
-                    updatedUsers.push(userId);
+                    // Make sure the game user is valid
+                    if(gameUser === null || gameUser === undefined) {
+                        // Respond with an error
+                        if(!cancelled)
+                            res.json({
+                                status: 'error',
+                                error: {
+                                    message: 'Invalid game and user ID combination for user \'' + userId + '\''
+                                }
+                            });
+
+                        // Set the cancelled flag and return
+                        cancelled = true;
+                        return;
+                    }
+
+                    // Determine the new team value
+                    var newTeam;
+
+                    // Set the new team value to none
+                    if(teamValue === 'none' || teamValue === '')
+                        newTeam = null;
+
+                    else
+                    // TODO: Determine the new team value (random, and team IDs)
+                        newTeam = null;
+
+                    // Create a fields object with the new field values
+                    const fields = {
+                        team: newTeam,
+                        is_special: isSpecial,
+                        is_spectator: isSpectator
+                    };
+
+                    // Set the users special state
+                    latch.add();
+                    gameUser.setFields(fields, function(err) {
+                        // Call back errors
+                        if(err !== null) {
+                            if(!cancelled)
+                                next(err);
+                            cancelled = true;
+                            return;
+                        }
+
+                        // Add the user to the updated users list
+                        updatedUsers.push(userId);
+
+                        // Resolve the latch
+                        latch.resolve();
+                    });
 
                     // Resolve the latch
                     latch.resolve();
                 });
-
-                // Resolve the latch
-                latch.resolve();
             });
-        });
 
-        // Send the result when we're done
-        latch.then(function() {
-            // Send an OK response if not cancelled
-            if(!cancelled)
-                res.json({
-                    status: 'ok',
-                    updatedUsers
-                });
+            // Send the result when we're done
+            latch.then(function() {
+                // Send an OK response if not cancelled
+                if(!cancelled)
+                    res.json({
+                        status: 'ok',
+                        updatedUsers
+                    });
+            });
         });
     });
 });
