474fcb2b2963e534178963226a1daab9cff8343a
Tim Visée <timvisee@gmail.com>
2016-09-10 16:58:17 +0200

    Show game stage on game and game info page

--- server/app/route/game.js
+++ server/app/route/game.js
@@ -90,6 +90,24 @@ router.get('/:game', function(req, res, next) {
         latch.resolve();
     });
 
+    // Fetch the game stage
+    latch.add();
+    game.getStage(function(err, stage) {
+        // Call back errors
+        if(err !== null) {
+            if(!calledBack)
+                next(err);
+            calledBack = true;
+            return;
+        }
+
+        // Set the property
+        gameObject.stage = stage;
+
+        // Resolve the latch
+        latch.resolve();
+    });
+
     // Fetch the game's users count
     latch.add();
     game.getUsersCount(function(err, usersCount) {
