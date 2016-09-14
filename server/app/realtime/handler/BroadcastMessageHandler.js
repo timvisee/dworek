2391eac66cb0871732db01fa8395a217fd28f0b7
Tim Visée <timvisee@gmail.com>
2016-09-14 02:41:38 +0200

    Remove duplicate code

--- server/app/realtime/handler/BroadcastMessageHandler.js
+++ server/app/realtime/handler/BroadcastMessageHandler.js
@@ -207,48 +207,6 @@ BroadcastMessageHandler.prototype.handler = function(packet, socket) {
                             });
                         });
                     });
-                    // Loop through the list of users
-                    users.forEach(function(user) {
-                        // Add the user, and their broadcast to the broadcast queue
-                        Core.realTime.queueBroadcast(broadcastObject, user.getIdHex().toLowerCase());
-                    });
-
-                    // Get the name of the game
-                    game.getName(function(err, gameName) {
-                        // Handle errors
-                        if(err !== null)
-                            gameName = 'Unknown';
-
-                        // Set the game name in the broadcast object
-                        broadcastObject.gameName = gameName;
-
-                        // Loop through all connected clients, to send the game stage update
-                        Object.keys(Core.realTime._io.sockets.sockets).forEach(function(socketId) {
-                            // Get the socket
-                            const entrySocket = Core.realTime._io.sockets.sockets[socketId];
-
-                            // Skip the socket if not authenticated
-                            if(!_.has(entrySocket, 'session.valid') || !_.has(entrySocket, 'session.user') || !entrySocket.session.valid)
-                                return;
-
-                            // Get the user
-                            const user = entrySocket.session.user;
-
-                            // Check whether the user joined this game
-                            game.hasUser(user, function(err, joined) {
-                                // Skip this socket if an error occurred
-                                if(err !== null)
-                                    return;
-
-                                // Make sure the user joined
-                                if(!joined)
-                                    return;
-
-                                // Send a broadcast packet to the user
-                                Core.realTime.packetProcessor.sendPacket(PacketType.BROADCAST_MESSAGE, broadcastObject, entrySocket);
-                            });
-                        });
-                    });
                 });
             });
         });
