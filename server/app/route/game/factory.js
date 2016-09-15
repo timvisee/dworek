f6666eacfc3f6b64e07815274b6e81942596f357
Tim Visée <timvisee@gmail.com>
2016-09-15 21:53:59 +0200

    Show additional factory properties on factory page

--- server/app/route/game/factory.js
+++ server/app/route/game/factory.js
@@ -141,6 +141,7 @@ module.exports = {
                         var latch = new CallbackLatch();
 
                         // Get the factory name
+                        latch.add();
                         factoryModel.getName(function(err, name) {
                             // Call back errors
                             if(err !== null) {
@@ -158,6 +159,7 @@ module.exports = {
                         });
 
                         // Get the factory level
+                        latch.add();
                         factoryModel.getLevel(function(err, level) {
                             // Call back errors
                             if(err !== null) {
@@ -169,6 +171,133 @@ module.exports = {
 
                             // Set the level
                             factoryObject.level = level;
+
+                            // Resolve the latch
+                            latch.resolve();
+                        });
+
+                        // Get the defence value
+                        latch.add();
+                        factoryModel.getDefence(function(err, defence) {
+                            // Call back errors
+                            if(err !== null) {
+                                if(!calledBack)
+                                    next(err);
+                                calledBack = true;
+                                return;
+                            }
+
+                            // Set the defence
+                            factoryObject.defence = defence;
+
+                            // Resolve the latch
+                            latch.resolve();
+                        });
+
+                        // Get the input
+                        latch.add();
+                        factoryModel.getIn(function(err, input) {
+                            // Call back errors
+                            if(err !== null) {
+                                if(!calledBack)
+                                    next(err);
+                                calledBack = true;
+                                return;
+                            }
+
+                            // Set the input
+                            factoryObject.in = input;
+
+                            // Resolve the latch
+                            latch.resolve();
+                        });
+
+                        // Get the output
+                        latch.add();
+                        factoryModel.getOut(function(err, output) {
+                            // Call back errors
+                            if(err !== null) {
+                                if(!calledBack)
+                                    next(err);
+                                calledBack = true;
+                                return;
+                            }
+
+                            // Set the defence
+                            factoryObject.out = output;
+
+                            // Resolve the latch
+                            latch.resolve();
+                        });
+
+                        // Get the creator
+                        latch.add();
+                        factoryModel.getUser(function(err, creator) {
+                            // Call back errors
+                            if(err !== null) {
+                                if(!calledBack)
+                                    next(err);
+                                calledBack = true;
+                                return;
+                            }
+
+                            // Get the display name of the user
+                            latch.add();
+                            creator.getDisplayName(function(err, displayName) {
+                                // Call back errors
+                                if(err !== null) {
+                                    if(!calledBack)
+                                        next(err);
+                                    calledBack = true;
+                                    return;
+                                }
+
+                                // Set the display name
+                                factoryObject.creatorName = displayName;
+
+                                // Resolve the latch
+                                latch.resolve();
+                            });
+
+                            // Get the game user
+                            latch.add();
+                            Core.model.gameUserModelManager.getGameUser(game, creator, function(err, gameUser) {
+                                // Call back errors
+                                if(err !== null) {
+                                    if(!calledBack)
+                                        next(err);
+                                    calledBack = true;
+                                    return;
+                                }
+
+                                // Get the team
+                                gameUser.getTeam(function(err, team) {
+                                    // Call back errors
+                                    if(err !== null) {
+                                        if(!calledBack)
+                                            next(err);
+                                        calledBack = true;
+                                        return;
+                                    }
+
+                                    // Get the team name
+                                    team.getName(function(err, teamName) {
+                                        // Call back errors
+                                        if(err !== null) {
+                                            if(!calledBack)
+                                                next(err);
+                                            calledBack = true;
+                                            return;
+                                        }
+
+                                        // Set the team name
+                                        factoryObject.teamName = teamName;
+
+                                        // Resolve the latch
+                                        latch.resolve();
+                                    });
+                                });
+                            });
 
                             // Resolve the latch
                             latch.resolve();
