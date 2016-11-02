d7e05b5e52502c6a347185edaa7260295cf06adc
Tim Visée <timvisee@gmail.com>
2016-11-02 20:38:40 +0100

    Make factory level upgrade unprofitable after level 10, not level 8

--- server/gameConfig.js
+++ server/gameConfig.js
@@ -330,11 +330,11 @@ var gameConfig = {
             const levelCost = Math.round(250 + 500 * Math.pow(level - 1, 1.5));
 
             // Return the level cost if the level is 8 or below
-            if(level <= 8)
+            if(level <= 10)
                 return levelCost;
 
             // Increase the level cost rapidly for higher levels
-            return Math.round(levelCost * Math.pow(1.2, level - 8));
+            return Math.round(levelCost * Math.pow(1.2, level - 10));
         },
 
         /**
